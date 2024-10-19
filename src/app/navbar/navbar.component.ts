81c689aadd0cf29de37dcb6c78593298aed28e0f
dtsivkovski <dtsivkovs@gmail.com>
2024-10-19 12:55:24 -0700

    navbar?

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,116 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MenubarModule, BadgeModule, AvatarModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  items: MenuItem[] = [
+          {
+              label:'My Plans',
+              icon:'pi pi-fw pi-file'
+          },
+          {
+              label:'Edit',
+              icon:'pi pi-fw pi-pencil',
+              items:[
+                  {
+                      label:'Left',
+                      icon:'pi pi-fw pi-align-left'
+                  },
+                  {
+                      label:'Right',
+                      icon:'pi pi-fw pi-align-right'
+                  },
+                  {
+                      label:'Center',
+                      icon:'pi pi-fw pi-align-center'
+                  },
+                  {
+                      label:'Justify',
+                      icon:'pi pi-fw pi-align-justify'
+                  },
+
+              ]
+          },
+          {
+              label:'Users',
+              icon:'pi pi-fw pi-user',
+              items:[
+                  {
+                      label:'New',
+                      icon:'pi pi-fw pi-user-plus',
+
+                  },
+                  {
+                      label:'Delete',
+                      icon:'pi pi-fw pi-user-minus',
+
+                  },
+                  {
+                      label:'Search',
+                      icon:'pi pi-fw pi-users',
+                      items:[
+                      {
+                          label:'Filter',
+                          icon:'pi pi-fw pi-filter',
+                          items:[
+                              {
+                                  label:'Print',
+                                  icon:'pi pi-fw pi-print'
+                              }
+                          ]
+                      },
+                      {
+                          icon:'pi pi-fw pi-bars',
+                          label:'List'
+                      }
+                      ]
+                  }
+              ]
+          },
+          {
+              label:'Events',
+              icon:'pi pi-fw pi-calendar',
+              items:[
+                  {
+                      label:'Edit',
+                      icon:'pi pi-fw pi-pencil',
+                      items:[
+                      {
+                          label:'Save',
+                          icon:'pi pi-fw pi-calendar-plus'
+                      },
+                      {
+                          label:'Delete',
+                          icon:'pi pi-fw pi-calendar-minus'
+                      },
+
+                      ]
+                  },
+                  {
+                      label:'Archieve',
+                      icon:'pi pi-fw pi-calendar-times',
+                      items:[
+                      {
+                          label:'Remove',
+                          icon:'pi pi-fw pi-calendar-minus'
+                      }
+                      ]
+                  }
+              ]
+          },
+          {
+              label:'Quit',
+              icon:'pi pi-fw pi-power-off'
+          }
+      ];
 }
