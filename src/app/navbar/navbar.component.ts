98b24638bbe56fbcf2dfa65ae5f09c80a1ea2168
dtsivkovski <dtsivkovs@gmail.com>
2024-10-19 13:45:19 -0700

    navbar temp

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { BadgeModule } from 'primeng/badge';
 import { AvatarModule } from 'primeng/avatar';
 import { MenuItem } from 'primeng/api';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MenubarModule, BadgeModule, AvatarModule],
+  imports: [MenubarModule, BadgeModule, AvatarModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
