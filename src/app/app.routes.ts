68d2d76bd1e3f8561c6cdc8a8054afb1489f2ecf
dtsivkovski <dtsivkovs@gmail.com>
2024-10-19 15:31:22 -0700

    angular is gone

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { OnboardingScreenComponent } from './onboarding-screen/onboarding-screen.component';
-import { PlansScreenComponent } from './plans-screen/plans-screen.component';
-
-export const routes: Routes = [
-    {path: 'onboarding', component: OnboardingScreenComponent},
-    {path: 'plans', component: PlansScreenComponent}
-];
